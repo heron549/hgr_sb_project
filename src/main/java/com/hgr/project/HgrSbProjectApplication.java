5fe94225b79f1a3f1c1281c8d677b566dd06bb23
heron549 <dlfrnfn123@gmail.com>
2024-08-29 16:10:16 +0900

    Init : springboot init

---
+++ src/main/java/com/hgr/project/HgrSbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hgr.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HgrSbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HgrSbProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hgr/project/HgrSbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgr.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HgrSbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
